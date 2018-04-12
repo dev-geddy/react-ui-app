17c412124427461bc4cce20e4fbfa2e2b2bef93b
Gediminas <gediminasgn@gmail.com>
2018-04-13 00:10:21 +0100

    feat: use component from ES6 lib

--- src/App/index.js
+++ src/App/index.js
@@ -1,11 +1,13 @@
 import React, {Component} from 'react'
 import styles from './style.scss'
+import SampleComponent from '@reactivelabs/react-ui-lib/src/App'
 
 class App extends Component {
   render() {
     return (
       <div className={styles.App}>
-        App example with component "Hi"
+        App example with component "Hi":
+        <SampleComponent />
       </div>
     )
   }
--- webpack.config.js
+++ webpack.config.js
@@ -20,6 +20,7 @@ module.exports = {
   module: {
     rules: [
       {
+        exclude: /node_modules\/(?!(@reactivelabs)\/)/,
         loader: 'babel-loader',
         test: /\.js$/
       },
