20de4705e7960a58cdbb716c6f106bba5ebe7e44
Gediminas <gediminasgn@gmail.com>
2018-04-13 00:09:29 +0100

    init: project with dev build config

---
+++ src/App/index.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react'
+import styles from './style.scss'
+
+class App extends Component {
+  render() {
+    return (
+      <div className={styles.App}>
+        App example with component "Hi"
+      </div>
+    )
+  }
+}
+
+export default App
